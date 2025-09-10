c8a0c377d12dc476d096a054d877159751184220
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-09-10 22:21:04 +0100

    WIP

--- src/parser/parser_dsl.ml
+++ src/parser/parser_dsl.ml
@@ -1,104 +1,112 @@
 open! Import
+open Isa
 
-type ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t = {
-  path : Path.t;
-  opcode_builder : ('reg, 'opcode, 'rel, 'out) Builder.t;
-  dir_builder : ('reg, 'dir, 'rel, 'out) Builder.t;
-  res_builder : ('reg, 'res, 'rel, 'rel Relocatable.t) Builder.t;
-  token_reader : ('reg, 'dir, 'opcode, 'res) Isa.Token.t Token_reader.t;
-  dgn_printer : Diagnostics_printer.t;
-}
-[@@deriving fields]
-
-let with_opcode_builder st opcode f =
-  Builder.start st.opcode_builder opcode;
-  f st.opcode_builder
-
-let with_dir_builder st dir f =
-  Builder.start st.dir_builder dir;
-  f st.dir_builder
-
-let add_register _st bldr reg = Builder.add_register bldr reg
-let add_rel _st bldr rel = Builder.add_rel bldr rel
-let add_string _st bldr str = Builder.add_string bldr str
-let add_base_offset _st bldr base off = Builder.add_base_offset bldr base off
-let build _st bldr = Builder.build bldr
-
-let next st =
-  let t = Token_reader.next st.token_reader in
-  t
-
-let peek st = Token_reader.peek st.token_reader
-
-let skip st =
-  let _ = Token_reader.next st.token_reader in
-  ()
-
-let last_token_info st = Token_reader.last_token_info st.token_reader
-
-let rec next_non_whitespace st =
-  let open Token in
-  match next st with White_space -> next_non_whitespace st | tok -> tok
-
-let rec peek_non_whitespace st =
-  let open Token in
-  match peek st with
-  | White_space ->
-      skip st;
-      peek_non_whitespace st
-  | tok -> tok
-
-let rec skip_whitespaces_and_newlines st =
-  let open Token in
-  match peek st with
-  | White_space | Eol ->
-      skip st;
-      skip_whitespaces_and_newlines st
-  | _ -> ()
-
-let pos st =
-  let open Token_info in
-  let token_info = Token_reader.last_token_info st.token_reader in
-  (token_info.starts, token_info.ends)
-
-let emit_diagnostic_message st err_or_warn =
-  let open Either in
-  let open Diagnostics_message in
-  let id, msg, ty =
-    match err_or_warn with
-    | First err ->
-        ( Diagnostics.Error.id err,
-          Diagnostics.Error.show err,
-          Diagnostics_type.Error )
-    | Second warn ->
-        ( Diagnostics.Warning.id warn,
-          Diagnostics.Warning.show warn,
-          Diagnostics_type.Warning )
-  in
-  let starts, ends = pos st in
-  let dgn_msg = { id; ty; msg; starts; ends; file = path st; ctx = "" } in
-  Diagnostics_printer.emit st.dgn_printer dgn_msg err_or_warn
-
-let error st err =
-  let open Result.Let_syntax in
-  let err = Either.First err in
-  (* We now that Diagnostics_printer will return Error anyway when we pass an
-     error. *)
-  let%bind () = emit_diagnostic_message st err in
-  Error err
-
-let warning st warn = emit_diagnostic_message st (Either.Second warn)
-
-let create ?(path = Path.empty) reg_m dir_m opcode_m res_m ~word_size
-    ~build_instruction ~build_directive ~build_reserved toks dgn_printer =
-  {
-    path;
-    opcode_builder =
-      Builder.create reg_m opcode_m ~word_size ~builder_fn:build_instruction;
-    dir_builder =
-      Builder.create reg_m dir_m ~word_size ~builder_fn:build_directive;
-    res_builder =
-      Builder.create reg_m res_m ~word_size ~builder_fn:build_reserved;
-    token_reader = Token_reader.create toks;
-    dgn_printer;
+module Mk
+    (Opcode : Expr.S)
+    (Direc : Expr.S)
+    (Reserved : Expr.S)
+    (Reg : Register.S)
+    (Reloc_data : T.T) =
+struct
+  type 'out t = {
+    path : Path.t;
+    opcode_builder : (Reg.t, Opcode.t, Reloc_data.t, 'out) Builder.t;
+    dir_builder : (Reg.t, Opcode.t, Reloc_data.t, 'out) Builder.t;
+    res_builder :
+      (Reg.t, Opcode.t, Reloc_data.t, Reloc_data.t Relocatable.t) Builder.t;
+    token_reader :
+      (Reg.t, Direc.t, Opcode.t, Reserved.t) Isa.Token.t Token_reader.t;
+    dgn_printer : Diagnostics_printer.t;
   }
+  [@@deriving fields]
+
+  let with_opcode_builder st opcode f =
+    Builder.start st.opcode_builder opcode;
+    f st.opcode_builder
+
+  let with_dir_builder st dir f =
+    Builder.start st.dir_builder dir;
+    f st.dir_builder
+
+  let add_register _st bldr reg = Builder.add_register bldr reg
+  let add_rel _st bldr rel = Builder.add_rel bldr rel
+  let add_string _st bldr str = Builder.add_string bldr str
+  let add_base_offset _st bldr base off = Builder.add_base_offset bldr base off
+  let build _st bldr = Builder.build bldr
+
+  let next st =
+    let t = Token_reader.next st.token_reader in
+    t
+
+  let peek st = Token_reader.peek st.token_reader
+
+  let skip st =
+    let _ = Token_reader.next st.token_reader in
+    ()
+
+  let last_token_info st = Token_reader.last_token_info st.token_reader
+
+  let rec next_non_whitespace st =
+    match next st with White_space -> next_non_whitespace st | tok -> tok
+
+  let rec peek_non_whitespace st =
+    match peek st with
+    | White_space ->
+        skip st;
+        peek_non_whitespace st
+    | tok -> tok
+
+  let rec skip_whitespaces_and_newlines st =
+    match peek st with
+    | White_space | Eol ->
+        skip st;
+        skip_whitespaces_and_newlines st
+    | _ -> ()
+
+  let pos st =
+    let open Token_info in
+    let token_info = Token_reader.last_token_info st.token_reader in
+    (token_info.starts, token_info.ends)
+
+  let emit_diagnostic_message st err_or_warn =
+    let open Either in
+    let open Diagnostics_message in
+    let id, msg, ty =
+      match err_or_warn with
+      | First err ->
+          ( Diagnostics.Error.id err,
+            Diagnostics.Error.show err,
+            Diagnostics_type.Error )
+      | Second warn ->
+          ( Diagnostics.Warning.id warn,
+            Diagnostics.Warning.show warn,
+            Diagnostics_type.Warning )
+    in
+    let starts, ends = pos st in
+    let dgn_msg = { id; ty; msg; starts; ends; file = path st; ctx = "" } in
+    Diagnostics_printer.emit st.dgn_printer dgn_msg err_or_warn
+
+  let error st err =
+    let open Result.Let_syntax in
+    let err = Either.First err in
+    (* We now that Diagnostics_printer will return Error anyway when we pass an
+       error. *)
+    let%bind () = emit_diagnostic_message st err in
+    Error err
+
+  let warning st warn = emit_diagnostic_message st (Either.Second warn)
+
+  let create ?(path = Path.empty) reg_m dir_m opcode_m res_m ~word_size
+      ~build_instruction ~build_directive ~build_reserved toks dgn_printer =
+    {
+      path;
+      opcode_builder =
+        Builder.create reg_m opcode_m ~word_size ~builder_fn:build_instruction;
+      dir_builder =
+        Builder.create reg_m dir_m ~word_size ~builder_fn:build_directive;
+      res_builder =
+        Builder.create reg_m res_m ~word_size ~builder_fn:build_reserved;
+      token_reader = Token_reader.create toks;
+      dgn_printer;
+    }
+end
--- src/parser/parser_dsl.mli
+++ src/parser/parser_dsl.mli
@@ -1,83 +1,77 @@
 open! Import
 
-type ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t
+module Mk
+    (Opcode : Isa.Expr.S)
+    (Direc : Isa.Expr.S)
+    (Reserved : Isa.Expr.S)
+    (Reg : Isa.Register.S)
+    (Reloc_data : T.T) : sig
+  type 'out t
 
-val create :
-  ?path:Path.t ->
-  'reg Isa.Register.t ->
-  'dir Isa.Expr.t ->
-  'opcode Isa.Expr.t ->
-  'res Isa.Expr.t ->
-  word_size:int ->
-  build_instruction:('reg, 'opcode, 'rel, 'out) Builder.Builder_fn.t ->
-  build_directive:('reg, 'dir, 'rel, 'out) Builder.Builder_fn.t ->
-  build_reserved:('reg, 'res, 'rel, 'rel Relocatable.t) Builder.Builder_fn.t ->
-  (('reg, 'dir, 'opcode, 'res) Isa.Token.t Token.t * Lexer.Token_info.t)
-  Sequence.t ->
-  Diagnostics_printer.t ->
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t
+  val create :
+    ?path:Path.t ->
+    'reg Isa.Register.t ->
+    'dir Isa.Expr.t ->
+    'opcode Isa.Expr.t ->
+    'res Isa.Expr.t ->
+    word_size:int ->
+    build_instruction:(Reg.t, Opcode.t, Reloc_data.t, 'out) Builder.Builder_fn.t ->
+    build_directive:(Reg.t, Opcode.t, Reloc_data.t, 'out) Builder.Builder_fn.t ->
+    build_reserved:
+      ( Reg.t,
+        Opcode.t,
+        Reloc_data.t,
+        Reloc_data.t Relocatable.t )
+      Builder.Builder_fn.t ->
+    ((Reg.t, Direc.t, Opcode.t, Reserved.t) Isa.Token.t Token.t
+    * Lexer.Token_info.t)
+    Sequence.t ->
+    Diagnostics_printer.t ->
+    'out t
 
-val with_opcode_builder :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  'opcode ->
-  (('reg, 'opcode, 'rel, 'out) Builder.t -> 'a) ->
-  'a
+  val with_opcode_builder :
+    'out t ->
+    Opcode.t ->
+    ((Reg.t, Opcode.t, Reloc_data.t, 'out) Builder.t -> 'a) ->
+    'a
 
-val with_dir_builder :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  'dir ->
-  (('reg, 'dir, 'rel, 'out) Builder.t -> 'a) ->
-  'a
+  val with_dir_builder :
+    'out t ->
+    'dir ->
+    ((Reg.t, Opcode.t, Reloc_data.t, 'out) Builder.t -> 'a) ->
+    'a
 
-val add_register :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'comm, 'rel, 'out) Builder.t ->
-  'reg ->
-  unit
+  val add_register :
+    'out t -> (Reg.t, 'comm, Reloc_data.t, 'out) Builder.t -> Reg.t -> unit
 
-val add_rel :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'comm, 'rel, 'out) Builder.t ->
-  'rel Relocatable.t ->
-  unit
+  val add_rel :
+    'out t ->
+    (Reg.t, 'comm, Reloc_data.t, 'out) Builder.t ->
+    Reloc_data.t Relocatable.t ->
+    unit
 
-val add_string :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'comm, 'rel, 'out) Builder.t ->
-  string ->
-  unit
+  val add_string :
+    'out t -> (Reg.t, 'comm, Reloc_data.t, 'out) Builder.t -> string -> unit
 
-val add_base_offset :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'comm, 'rel, 'out) Builder.t ->
-  'reg ->
-  'rel Relocatable.t ->
-  unit
+  val add_base_offset :
+    'out t ->
+    (Reg.t, 'comm, Reloc_data.t, 'out) Builder.t ->
+    Reg.t ->
+    Reloc_data.t Relocatable.t ->
+    unit
 
-val build :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'comm, 'rel, 'out) Builder.t ->
-  'out
+  val build : 'out t -> (Reg.t, 'comm, Reloc_data.t, 'out) Builder.t -> 'out
+  val next : _ t -> (Reg.t, Direc.t, Opcode.t, Reserved.t) Isa.Token.t Token.t
+  val peek : _ t -> (Reg.t, Direc.t, Opcode.t, Reserved.t) Isa.Token.t Token.t
+  val skip : _ t -> unit
 
-val next :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'dir, 'opcode, 'res) Isa.Token.t Token.t
+  val next_non_whitespace :
+    _ t -> (Reg.t, Direc.t, Opcode.t, Reserved.t) Isa.Token.t Token.t
 
-val peek :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'dir, 'opcode, 'res) Isa.Token.t Token.t
+  val skip_whitespaces_and_newlines : _ t -> unit
 
-val skip : ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t -> unit
+  val peek_non_whitespace :
+    _ t -> (Reg.t, Direc.t, Opcode.t, Reserved.t) Isa.Token.t Token.t
 
-val next_non_whitespace :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'dir, 'opcode, 'res) Isa.Token.t Token.t
-
-val skip_whitespaces_and_newlines :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t -> unit
-
-val peek_non_whitespace :
-  ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t ->
-  ('reg, 'dir, 'opcode, 'res) Isa.Token.t Token.t
-
-val last_token_info : ('reg, 'dir, 'opcode, 'res, 'rel, 'out) t -> Token_info.t
+  val last_token_info : _ t -> Token_info.t
+end
